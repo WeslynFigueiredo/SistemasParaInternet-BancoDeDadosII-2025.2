fbbda137956d022174e40101e6019a27f5c7f30d
clayviana <clayviana24@gmail.com>
2025-10-13 20:43:34 -0300

    Update EXERCICIO6.sql

--- Atividade 1: Prática de SQL DDL/Clay_Santos/EXERCICIO6.sql
+++ Atividade 1: Prática de SQL DDL/Clay_Santos/EXERCICIO6.sql
@@ -2,14 +2,14 @@ CREATE database blog_de_noticias;
 
 use blog_de_noticias;
 create table autores (
-id_autor INT primary KEY,
-nome varchar(100) not null);
+id_autor INT PRIMARY KEY,
+nome varchar(100) NOT NULL);
 
 create table post(
-id_post INT primary KEY,
-titulo varchar(150) not null,
-conteudo text not null,
-data_publicacao timestamp not null,
+id_post INT PRIMARY KEY,
+titulo varchar(150) NOT NULL,
+conteudo text NOT NULL,
+data_publicacao DEFAULT CURRENT_TIMESTAMP NOT NULL ,
 id_autor int,
 FOREIGN KEY (id_autor) REFERENCES autores(id_autor)
 );
