896076e28cabd49aed5099586dbb07f6e6114f43
Cássio Moura <71567876+C4SS1NH0@users.noreply.github.com>
2025-10-13 20:40:22 -0300

    Update Questão-6.sql

--- Atividade 1: Prática de SQL DDL/Questões-weslyn/Questão-6.sql
+++ Atividade 1: Prática de SQL DDL/Questões-weslyn/Questão-6.sql
@@ -1,7 +1,13 @@
-CREATE TABLE pedidos (
-    id_pedido INT PRIMARY KEY,
-    id_cliente INT,
-    data_pedido DATE NOT NULL DEFAULT CURRENT_DATE,
-    valor_total DECIMAL(10, 2) NOT NULL,
-    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+CREATE TABLE autores (
+    id_autor INT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE posts (
+    id_post INT PRIMARY KEY,
+    titulo VARCHAR(150) NOT NULL,
+    conteudo TEXT NOT NULL,
+    data_publicacao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    id_autor INT,
+    FOREIGN KEY (id_autor) REFERENCES autores(id_autor)
 );
