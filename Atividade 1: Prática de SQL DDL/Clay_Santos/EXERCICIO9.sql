0bd55ff56ced4a94fe7770951f8390268b1c1273
clayviana <clayviana24@gmail.com>
2025-10-13 20:46:16 -0300

    Update EXERCICIO9.sql

--- Atividade 1: Prática de SQL DDL/Clay_Santos/EXERCICIO9.sql
+++ Atividade 1: Prática de SQL DDL/Clay_Santos/EXERCICIO9.sql
@@ -1,14 +1,14 @@
 CREATE database Redes_sociais;
 use redes_sociais;
 create table usuarios (
-id_usuarios int primary key auto_increment,
-nome_usuario varchar(50) unique not null,
-email varchar(50) unique not null,
-data_cadastro timestamp);
+id_usuarios int PRIMARY KEY auto_increment,
+nome_usuario varchar(50) UNIQUE NOT NULL,
+email varchar(50) UNIQUE NOT NULL,
+data_cadastro NOT NULL DEFAULT CURRENT_TIMESTAMP);
 create table post (
-id_post int primary key,
+id_post int PRIMARY KEY,
 id_usuario int,
-conteudo text not null,
-data_post timestamp not null,
- FOREIGN KEY(id_usuario) references usuarios(id_usuarios)
+conteudo text NOT NULL,
+data_post timestamp NOT NULL,
+ FOREIGN KEY(id_usuario) REFERENCES usuarios(id_usuarios)
  );
