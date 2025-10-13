2866a37b5d1ff9eccae8b0f05a5789e77538e22c
Maick Santos <mickysantos20@gmail.com>
2025-10-13 19:59:28 -0300

    Update Questao4.sql

--- Atividade 1: Prática de SQL DDL/Maick/Questao4.sql
+++ Atividade 1: Prática de SQL DDL/Maick/Questao4.sql
@@ -3,7 +3,7 @@ CREATE DATABASE academia_ginastica;
 USE academia_ginastica;
 
 CREATE TABLE alunos (
-	  id_aluno INT AUTO_INCREMENT PRIMARY KEY,
+	id_aluno INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     data_nascimento DATE,
     email VARCHAR(50) UNIQUE,
