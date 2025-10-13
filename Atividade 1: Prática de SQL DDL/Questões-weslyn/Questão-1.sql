71f1015ed69ae217fbb58883b07006cab05e04b8
Cássio Moura <71567876+C4SS1NH0@users.noreply.github.com>
2025-10-13 20:35:38 -0300

    Update and rename Questão-1 to Questão-1.sql

---
+++ Atividade 1: Prática de SQL DDL/Questões-weslyn/Questão-1.sql
@@ -0,0 +1,8 @@
+Questão 1:
+CREATE TABLE produtos (
+    id_produto INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    descricao VARCHAR(255),
+    preco DECIMAL(10, 2) NOT NULL,
+    estoque INT NOT NULL DEFAULT 0
+);
