a70b4f323d63fa7d904b885abc283b0f99120ecc
Cássio Moura <71567876+C4SS1NH0@users.noreply.github.com>
2025-10-13 20:42:03 -0300

    Create Questão-8.sql

---
+++ Atividade 1: Prática de SQL DDL/Questões-weslyn/Questão-8.sql
@@ -0,0 +1,21 @@
+CREATE TABLE pacientes (
+    id_paciente INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100) NOT NULL,
+    cpf VARCHAR(11) UNIQUE NOT NULL,
+    data_nascimento DATE
+);
+
+CREATE TABLE medicos (
+    crm VARCHAR(15) PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    especialidade VARCHAR(50)
+);
+
+CREATE TABLE consultas (
+    id_consulta INT PRIMARY KEY,
+    id_paciente INT,
+    crm_medico VARCHAR(15),
+    data_hora DATETIME NOT NULL,
+    FOREIGN KEY (id_paciente) REFERENCES pacientes(id_paciente),
+    FOREIGN KEY (crm_medico) REFERENCES medicos(crm)
+);
