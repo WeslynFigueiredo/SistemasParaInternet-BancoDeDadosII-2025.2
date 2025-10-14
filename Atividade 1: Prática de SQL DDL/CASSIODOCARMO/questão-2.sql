ce6eea4879bc0a3ea82582726b3ca012cd58a11c
Cássio Gabriel Barroso Do Carmo <166414745+bydemons@users.noreply.github.com>
2025-10-13 21:11:10 -0300

    Create questão-2.sql

---
+++ Atividade 1: Prática de SQL DDL/CASSIODOCARMO/questão-2.sql
@@ -0,0 +1,7 @@
+CREATE TABLE livros (
+    id_livro INT PRIMARY KEY,
+    titulo VARCHAR(100) NOT NULL,
+    autor VARCHAR(100) NOT NULL,
+    ano_publicacao INT,
+    isbn VARCHAR(20) UNIQUE NOT NULL
+);
