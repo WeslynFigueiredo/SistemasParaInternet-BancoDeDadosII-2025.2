ecf962a4e2fe99958185138f73aac4d407fd60a3
Cássio Gabriel Barroso Do Carmo <166414745+bydemons@users.noreply.github.com>
2025-10-13 21:14:48 -0300

    Update questão-5.sql

--- Atividade 1: Prática de SQL DDL/CASSIODOCARMO/questão-5.sql
+++ Atividade 1: Prática de SQL DDL/CASSIODOCARMO/questão-5.sql
@@ -4,3 +4,11 @@ CREATE TABLE clientes (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL
 );
+-- Tabela de pedidos
+CREATE TABLE pedidos (
+    id_pedido INT PRIMARY KEY,
+    id_cliente INT,
+    data_pedido DATE NOT NULL DEFAULT CURRENT_DATE,
+    valor_total DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
+);
